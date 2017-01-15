8ca0add449345f7d507f2a691af6a22b19ed8495
kikot <kikot@DESKTOP-OF5176S>
2017-01-15 20:15:59 +0200

    Add project files.

---
+++ FunctionalProgramming/FunctionalProgramming.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace FunctionalProgramming
+{
+    class FunctionalProgramming
+    {
+        static void Main()
+        {
+            Person person = GetById(-5);
+            DisplayValue(person.Address.Street.Name);
+        }
+
+        private static void DisplayValue(string value)
+        {
+            throw new Exception();
+        }
+
+        private static Person GetById(int id)
+        {
+            if (id < 0)
+            {
+                return null;
+            }
+
+            return new Person(id, "Scarlett Johansson")
+            {
+            };
+        }
+    }
+
+    internal class Person
+    {
+        public Person(int id, string fullname)
+        {
+            this.Id = id;
+            this.FullName = fullname;
+        }
+
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public Address(Street street)
+        {
+            this.Street = street;
+        }
+
+        public Street Street { get; set; }
+        public int No { get; set; }
+    }
+
+    public class Street
+    {
+        public Street(string name)
+        {
+            this.Name = name;
+            if (name == null)
+            {
+                this.Name = "No street name";
+            }
+        }
+
+        public string Name { get; private set; }
+    }
+}
